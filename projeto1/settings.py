0784d328845fd98779609778019967bc08122cf5
adyelson <adyelson.loureiro@gmail.com>
2023-05-18 16:55:32 -0300

    aforavai

--- projeto1/settings.py
+++ projeto1/settings.py
@@ -130,9 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/' # dev
+STATIC_URL = '/static/' # dev
 STATIC_ROOT = BASE_DIR / 'staticfiles' # prod
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
