d1691de39265d443b7a09c45a020d3f5e54d31e0
adyelson <adyelson.loureiro@gmail.com>
2023-05-18 23:06:40 -0300

    newtest

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
+
 from .views import index, contato, produto
 
 urlpatterns = [
     path('', index, name='index'),
     path('contato/', contato, name='contato'),
-    path('produto/', produto, name='produto'),
+    path('produto/', produto, name='produto')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
